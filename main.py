aad81b3a86416d07cb1f3b45711e7d5ccf7a7cf0
shahzaibk23 <shahzaibceo@gmail.com>
2022-09-16 12:54:21 +0500

    add file trigger added

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-import eel
+import eel,tkinter, tkinter.filedialog as tkFileDialog, os
 from eel import init, start
+
+
+global source_files
+source_files = []
+
+@eel.expose
+def getSourceFile():
+    root = tkinter.Tk()
+    root.attributes("-topmost", True)
+    root.withdraw()
+    ask_source_files = tkFileDialog.askopenfilenames()
+    global source_files
+    source_files += root.tk.splitlist(ask_source_files)
+    # updateSourceFiles()
+    print(source_files)
+    eel.updateSourceFilesJS()
+
+# def updateSourceFiles():
+    # eel.updateSourceFilesJS(source_files)
 
 if __name__ == '__main__':
     init('web')
