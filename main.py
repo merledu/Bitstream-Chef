9931b3831bb301e4e4aa4bcc990b1e98d0043a2c
shahzaibk23 <shahzaibceo@gmail.com>
2022-09-18 03:04:03 +0500

    mainFile corrected

--- main.py
+++ main.py
@@ -90,4 +90,4 @@ def exportBitstream():
 
 if __name__ == '__main__':
     init('web')
-    start('main.html', mode='custom', cmdline_args=['/Users/shahzaibkashif/Bitstream-Chef/node_modules/electron/dist/Electron.app/Contents/MacOS/Electron', '.'], port=8007)
+    start('main.html', mode='custom', cmdline_args=['node_modules/electron/dist/electron', '.'], port=8007)
