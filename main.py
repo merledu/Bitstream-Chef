22b2a90aba154d4f087286a6f0a87030a773a751
shahzaibk23 <shahzaibceo@gmail.com>
2022-09-17 20:10:17 +0500

    map fpga screen integrated

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import eel,tkinter, tkinter.filedialog as tkFileDialog, os
+import eel
+import tkinter, tkinter.filedialog as tkFileDialog, os
 from eel import init, start
+from fpga import *
 
 
 global source_files
@@ -15,11 +17,28 @@ def getSourceFile():
     source_files += root.tk.splitlist(ask_source_files)
     # updateSourceFiles()
     print(source_files)
-    eel.updateSourceFilesJS()
+    eel.updateSourceFilesJS([(i,os.path.basename(i)) for i in source_files])
 
-# def updateSourceFiles():
-    # eel.updateSourceFilesJS(source_files)
+@eel.expose
+def removeSourceFile(file):
+    global source_files
+    source_files.remove(file)
+    eel.updateSourceFilesJS([(i,os.path.basename(i)) for i in source_files])
+
+@eel.expose
+def bringOutFPGAMapping():
+    eel.insertTopModules(["test_top1", "test_top2"])
+    eel.showFPGAMapping()
+
+@eel.expose
+def selectTop(top, fpga):
+    print(top, fpga)
+    # extract pins from top module..
+    if fpga == "arty-a7-35t":
+        eel.mapInputs(["test_in1", "test_in2"],list(ARTY_COMPS_i.keys()))
+        eel.mapOutputs(["test_out1", "test_out2"],list(ARTY_COMPS_o.keys()))
+
 
 if __name__ == '__main__':
     init('web')
-    start('main.html', mode='custom', cmdline_args=['node_modules/electron/dist/electron', '.'], port=8007)
+    start('main.html', mode='custom', cmdline_args=['/Users/shahzaibkashif/Bitstream-Chef/node_modules/electron/dist/Electron.app/Contents/MacOS/Electron', '.'], port=8007)
